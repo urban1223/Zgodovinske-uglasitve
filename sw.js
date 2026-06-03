5b8ca8e77a5b5b24725b7a871c2b0243f72d9a03
Urban Klancar <urby.klancar@gmail.com>
2026-06-03 15:57:26 +0200

    Prva različica tuner aplikacije

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'na-tuner-v1';
+const ASSETS = [
+  'index.html',
+  'aron.html',
+  'earlyfrench.html',
+  'just.html',
+  'kirnberger.html',
+  'pytagorean.html',
+  'rameau.html',
+  'rousseau.html',
+  'valotti.html',
+  'violin.html',
+  'werkmeister.html',
+  'meantone.html',
+  'meantone16.html'
+];
+
+// Shranjevanje datotek v lokalni cache ob namestitvi
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivacija in brisanje starih verzij
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// Mreža ali Cache (Offline delovanje)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
